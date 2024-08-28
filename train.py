d2980f531b4b313e51e9f06d7007574d321d02f5
mzulian00 <mzulian00@gmail.com>
2024-08-28 16:13:08 +0200

    Update train.py

--- train.py
+++ train.py
@@ -239,6 +239,9 @@ def save_model(generator, discriminator, name):
 	print('current dir : ', os.getcwd())
 	os.chdir(os.path.join('..'))
 	print('current dir : ', os.getcwd())
+	print('lista', os.listdir())
+	print('lista', [s for s in os.listdir() if s.startswith('My')])
+	print('primo', [s for s in os.listdir() if s.startswith('My')][0])
 	os.chdir( [s for s in os.listdir() if s.startswith('My')][0] )
 	print('current dir : ', os.getcwd())
 
